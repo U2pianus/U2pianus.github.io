f85d0000b27a14a774e6659a15bc69c3881629eb
U2pianus <2372696060@qq.com>
2025-09-17 20:07:19 +0800

    Add files via upload

---
+++ quiz app/script.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: yang-chaofan 2372696060@qq.com
+ * @Date: 2025-09-15 17:57:35
+ * @LastEditors: yang-chaofan 2372696060@qq.com
+ * @LastEditTime: 2025-09-15 18:10:58
+ * @FilePath: \vscode\quiz app\script.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ *           佛曰:  
+ *                   写字楼里写字间，写字间里程序员；  
+ *                   程序人员写程序，又拿程序换酒钱。  
+ *                   酒醒只在网上坐，酒醉还来网下眠；  
+ *                   酒醉酒醒日复日，网上网下年复年。  
+ *                   但愿老死电脑间，不愿鞠躬老板前；  
+ *                   奔驰宝马贵者趣，公交自行程序员。  
+ *                   别人笑我忒疯癫，我笑自己命太贱；  
+ *                   不见满街漂亮妹，哪个归得程序员？
+ */
+
+const quizData = [
+    {
+        question:"中国的全程是什么？",
+        a:""
+    }
+]
